d4a1ac019004d2470c95e10168f13524de57a934
Anusha Konatala <anu.konatala@gmail.com>
2018-09-02 06:42:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function nameToAttributes(drivers) {
 function attributesToPhrase(drivers){
   newArr = drivers.map(function (driver) {
   
-  return `${driver.name} is from ${driver.hometown}`)};
-  return newArr
+  return `${driver.name} is from ${driver.hometown}`
+  });
+  return newArr;
 }

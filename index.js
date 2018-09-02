3c0c4b248922ef40f0510ee105f5043123ddc9f1
Anusha Konatala <anu.konatala@gmail.com>
2018-09-02 06:33:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function attributesToPhrase(drivers){
   let findString = " is from ";
   let len = findString.length;
   newArr = drivers.map(function (driver) {
-  const firstLast = driver.split(" ");
-  return {name: firstLast[0], lastName: firstLast[1]}
+  let index = driver.split(" ");
+  return {name: firstLast[0], hometown: firstLast[1]}
    });
 }
